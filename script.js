0f798c1e6284ee75d8693d30c4a158c20de09074
ApekTahua <kelvin.hartono@binus.ac.id>
2025-02-14 21:00:59 +0700

    Initial Skeleton for Portfolio Site

---
+++ script.js
@@ -0,0 +1,10 @@
+// Javascript  = Interactive
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
